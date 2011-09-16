42aacd94e074bf42ae2109dfbc71aed586a79f41
Hani Suleiman <hani@formicary.net>
2011-09-16 16:04:17 -0400

    Start up the web service in tests, and add a test to dump out the wsdl

--- src/test/java/com/formicary/wstemplate/CatServiceTest.java
+++ src/test/java/com/formicary/wstemplate/CatServiceTest.java
@@ -1,6 +1,15 @@
 package com.formicary.wstemplate;
 
+import java.io.IOException;
+import javax.xml.ws.Endpoint;
+
 import com.formicary.wstemplate.model.Cat;
+import org.apache.commons.io.IOUtils;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +28,16 @@ import static org.junit.Assert.*;
 public class CatServiceTest {
   @Autowired
   private CatService service;
+  @Autowired
+  private Endpoint endpoint;
+  private static final HttpClient client = new DefaultHttpClient();
+
+  @Test
+  public void getWSDL() throws IOException {
+    HttpGet get = new HttpGet(((EndpointImpl)endpoint).getAddress() + "?wsdl");
+    HttpResponse response = client.execute(get);
+    System.out.println(IOUtils.toString(response.getEntity().getContent()));
+  }
 
   @Test
   public void createCatName() {
