80a73b3fe426b2d1865e20a521f3782ad6ef683f
Kevin Judson <kevin@kjudson.com>
2012-11-25 18:47:08 +0000

    Rename rest client

--- src/test/java/com/formicary/wstemplate/IntegrationTests.java
+++ src/test/java/com/formicary/wstemplate/IntegrationTests.java
@@ -25,7 +25,7 @@ public class IntegrationTests {
   private CatService soapService;
 
   @Autowired
-  @Qualifier("catclient")
+  @Qualifier("catclientrest")
   private CatService restService;
 
   @Test
